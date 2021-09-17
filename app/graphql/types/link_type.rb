68f37e56a20d7f0a568ed4397b7c17354b7115ba
Mārtiņš Krūze <martinskruze@Martinss-MacBook-Pro.local>
2021-09-17 15:32:58 +0300

    mutation for creating linkl

--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -1,7 +1,9 @@
 module Types
   class LinkType < Types::BaseObject
-    field :id, ID, null: false
-    field :url, String, null: false
-    field :description, String, null: false
+    description "Link - the magic of the internets"
+
+    field :id, ID, 'identifier', null: false
+    field :url, String, 'url', null: false
+    field :description, String, 'full description', null: false
   end
 end
--- db/migrate/20210917112026_create_links.rb
+++ db/migrate/20210917112026_create_links.rb
@@ -1,8 +1,8 @@
 class CreateLinks < ActiveRecord::Migration[6.1]
   def change
     create_table :links do |t|
-      t.string :url
-      t.text :description
+      t.string :url, description: 'URL of the link'
+      t.text :description, description: 'Link description'
 
       t.timestamps
     end
